432bfe93646396254032b8a33ffd20785e0f6c7d
Tom McClain <tiutmcclain@gmail.com>
2015-11-26 16:27:31 -0500

    Thank You with corrected input.

---
+++ ThankYou/ThankYou.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+myname = 'Ed'
+myage = 30
+# yourname = 'gg'
+# yourage = 5
+yourname = raw_input('What is your name? ')
+yourage = raw_input('How old are you? ')
+print "My name is " + myname + ", and I am " + str(myage) + " years old."
+print "Your name is " + yourname + ", and you are " + str(yourage) + "."
+print "Thank you for buying me a coffee, " +  yourname +  "!"
