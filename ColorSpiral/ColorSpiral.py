6d09c25bd09ea05611751aab818e32df4210e161
Tom McClain <tiutmcclain@gmail.com>
2015-11-26 15:45:19 -0500

    ColorSpiral - no side variables

---
+++ ColorSpiral/ColorSpiral.py
@@ -0,0 +1,11 @@
+import turtle
+colors = ['red','yellow','blue','orange','green','purple']
+sides=6
+# changes 6 down to 2
+t=turtle.Pen()
+turtle.bgcolor("black")
+for x in range(360):
+    t.pencolor(colors[x%sides])
+    t.forward(x * 3/sides + x)
+    t.left(360/sides + 1)
+    t.width(x*sides/200)
